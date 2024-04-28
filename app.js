e8bce02c1c3a444cc31b9268fbbe36e23f76753f
Obitope Eniola Nathaniel <obitopeeniola@gamil.com>
2024-04-28 06:55:47 +0100

    del-button

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const date = new Date();
+document.getElementById("date").innerHTML = date;
+
+let display = document.getElementById("display")
+let button = document.querySelectorAll(".btn")
+button.forEach((item) => {
+    item.addEventListener("click", () => {
+        if(item.innerText === "="){
+            try{
+                let result = parseFloat(eval(display.value))
+                display.value = result
+                return
+            }catch{
+                display.value = display.value
+                return
+            }
+        }
+        if(item.innerText === "C"){
+            display.value = ""
+            return
+        }
+        display.value += item.innerText
+    })
+   
+})
+
--- app1.js
+++ app1.js
@@ -3,14 +3,16 @@ const day = date.getDate();
 const month = date.getMonth();
 const year = date.getFullYear();
 
-
+const sound1 = new Audio("click.wav")
 
 document.getElementById("date").innerHTML = `${day}-${month + 1}-${year}`;
 
 let display= document.getElementById("display")
 let button = document.querySelectorAll(".btn")
+
 button.forEach((item) => {
     item.addEventListener("click",() => {
+        sound1.play()
         if(item.innerText === "="){            
             try{                
                 let result = parseFloat(eval(display.value))          
@@ -24,6 +26,12 @@ button.forEach((item) => {
         if(item.innerText === "C"){        
             display.value = ""
             return           
+        }
+        if(item.innerText === "del"){
+            currentChar = display.value
+            lastChar = currentChar.slice(0, currentChar.length - 1)
+            display.value = lastChar
+            return
         }
         display.value += item.innerText        
     })
